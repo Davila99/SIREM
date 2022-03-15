24d7dfca2cc83b2eda35a3810172be5974de415c
Davila99 <davilaeliseo453@gmail.com>
2022-03-14 22:52:31 -0600

    Creacion del modelo, controlador y migracion Profesion

---
+++ app/Http/Controllers/CargoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cargo;
+use Illuminate\Http\Request;
+
+class CargoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cargo  $cargo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cargo $cargo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cargo  $cargo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cargo $cargo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cargo  $cargo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cargo $cargo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cargo  $cargo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cargo $cargo)
+    {
+        //
+    }
+}
