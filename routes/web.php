3bae1ae4380b0e85ab2164f931814f247935c67d
Stefany-Herrera <karen07stefanyh@gmail.com>
2022-03-31 19:59:12 -0600

    modificaciones

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::resource('cargos', CargoController::class);
