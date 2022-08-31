3344d48caf56f4f3d71ac6713e993b5acd729b73
Davila99 <davilaeliseo453@gmail.com>
2022-08-31 19:06:26 -0400

    Cambio de titulo en la vista de tipoMatricula

--- resources/views/tipoMatricula/create.blade.php
+++ resources/views/tipoMatricula/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <h1>Registrar Nueva Matriculas</h1>
+    <h1>Registrar Nuevo Tipo de Matricula</h1>
     <form action="{{ url('/tmatricula') }}" method="post" enctype="multipart/form-data">
     @csrf
 
