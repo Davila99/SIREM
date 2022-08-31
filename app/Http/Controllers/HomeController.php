e5eb58b9f5a3a9bf0ab7166da9fa950c9bdf464e
Davila99 <davilaeliseo453@gmail.com>
2022-08-30 20:20:07 -0400

    Modificaciones en los requerimientos del proyecto y modificaciones en el controlador HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Estudiante;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $datos['estudiantes'] = Estudiante::query()
+        ->with(['tutor'])
+        ->with(['sexo'])
+        ->paginate(5);
+        return view('estudiante/index', $datos);
     }
 }
