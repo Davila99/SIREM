e24933ac3209beb93bc74eb3f81115c2a764a154
Davila99 <davilaeliseo453@gmail.com>
2023-03-05 12:52:27 -0600

    integrando alert en la vista organizacion academica

--- resources/views/organizacionacademica/index.blade.php
+++ resources/views/organizacionacademica/index.blade.php
@@ -22,15 +22,20 @@
                     <td>{{ $organizacionacademica->descripcion }}</td>
                     <td>{{ $organizacionacademica->fecha }}</td>
                     <td>
-                        <input data-id="{{$organizacionacademica->id}}" class="toggle-class" type="checkbox" data-onstyle="success" data-offstyle="danger" data-toggle="toggle" data-on="Active" data-off="InActive" {{ $organizacionacademica->confirmed ? 'checked' : '' }}>
+                        <input data-id="{{ $organizacionacademica->id }}" class="toggle-class" type="checkbox"
+                            data-onstyle="success" data-offstyle="danger" data-toggle="toggle" data-on="Active"
+                            data-off="InActive" {{ $organizacionacademica->confirmed ? 'checked' : '' }}>
                     </td>
                     <td>
                         <div class="d-flex flex-row bd-highlight mb-6">
-                            <a href="{{ url('/organizacionacademica/' . $organizacionacademica->id . '/edit') }}" class="btn btn-info">
+                            <a href="{{ url('/organizacionacademica/' . $organizacionacademica->id . '/edit') }}"
+                                class="btn btn-info">
                                 Editar </a>|
-                                <a href="{{ url('/organizacionacademica/' . $organizacionacademica->id) }}" class="btn btn-warning">
-                                    Detalles </a>|
-                            <form class="form-eliminar" action="{{ url('/organizacionacademica/' . $organizacionacademica->id) }}" method="post"
+                            <a href="{{ url('/organizacionacademica/' . $organizacionacademica->id) }}"
+                                class="btn btn-warning">
+                                Detalles </a>|
+                            <form class="form-eliminar"
+                                action="{{ url('/organizacionacademica/' . $organizacionacademica->id) }}" method="post"
                                 class="d-inline">
                                 @csrf
                                 {{ method_field('DELETE') }}
@@ -64,7 +69,7 @@
             Swal.fire({
                 position: 'top-center',
                 icon: 'success',
-                title: 'Profesión editado exitosamente!',
+                title: 'Organización Academica editado exitosamente!',
                 showConfirmButton: false,
                 timer: 2000
             })
@@ -111,21 +116,39 @@
     </script>
     <script>
         $(function() {
-          $('.toggle-class').change(function() {
-              var confirmed = $(this).prop('checked') == true ? 1 : 0; 
-              var organizacionacademica = $(this).data('id'); 
-               
-              $.ajax({
-                  type: "GET",
-                  dataType: "json",
-                  url: '/changeStatus',
-                  data: {'confirmed': confirmed, 'organizacionacademica': organizacionacademica},
-                  success: function(data){
-                    console.log(data.success)
-                  }
-              });
-          })
+            $('.toggle-class').change(function() {
+                Swal.fire({
+                    title: '¿Estas seguro?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Autorizar',
+                    cancelButtonText: 'Calcelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        var confirmed = $(this).prop('checked') == true ? 1 : 0;
+                        var organizacionacademica = $(this).data('id');
+                        $.ajax({
+                            type: "GET",
+                            dataType: "json",
+                            url: '/changeStatus',
+                            data: {
+                                'confirmed': confirmed,
+                                'organizacionacademica': organizacionacademica
+                            },
+                            success: function(data) {
+                                location.reload();
+                                console.log(data.success)
+                            }
+                        });
+                    }else{
+                        location.reload();
+                    }
+                })
+
+            })
         })
-      </script>
+    </script>
 
 @endsection
