53c9c48fdb06550249493eb75a6e60b0b3f33f70
Davila99 <davilaeliseo453@gmail.com>
2023-03-18 17:25:55 -0600

    Update BuscadorEstudiantes.jsx

--- resources/js/components/estudiantes/BuscadorEstudiantes.jsx
+++ resources/js/components/estudiantes/BuscadorEstudiantes.jsx
@@ -1,52 +1,59 @@
-
-import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 function BuscadorEstudiantes() {
-    const [ users, setUsers ] = useState([])
-    const [ estudiantes, setEstudiantes ] = useState([])
-    const [ search, setSearch ] = useState("")
-  
+    const numbers = [1, 2, 3, 4, 5];
+    const [estudiantes, setEstudiantes] = useState([]);
+    const [search, setSearch] = useState("");
+    useEffect(() => {
+        getEstudiantes();
+    }, []);
     //función para traer los datos de la API
-    const URL = 'http://localhost:8000/api/estudiantes'
-  
-    const showData = async () => {
-      const response = await fetch(URL)
-      const data = await response.json()
-      console.log(data)
-      setUsers(data)
-    }   
-     //función de búsqueda
+    const URL = "http://127.0.0.1:8000/search-estudiantes";
+
+    const getEstudiantes = async () => {
+        const respuesta = await axios.get(URL);
+        setEstudiantes(respuesta.data);
+    };
+
+    //función de búsqueda
     const searcher = (e) => {
-        setSearch(e.target.value)   
-    }
-     //metodo de filtrado 2   
-     const results = !search ? estudiantes : estudiantes.filter((dato)=> dato.nombres.toLowerCase().includes(search.toLocaleLowerCase()))
-     console.log(results)
-     useEffect( ()=> {
-    showData()
-      
-    }, [])
-    return ( 
+        setSearch(e.target.value);
+    };
+    //metodo de filtrado 2
+    const results = !search
+        ? estudiantes
+        : estudiantes.data.filter((dato) =>
+              dato.nombres.toLowerCase().includes(search.toLocaleLowerCase())
+          );
+
+    return (
         <div>
-        <input value={search} onChange={searcher} type="text" placeholder='Search' className='form-control'/>
-    
-        <table className='table table-striped table-hover mt-5 shadow-lg'>
-            <thead>
-                <tr className='bg-curso text-white'>
-                    <th>Nombre</th>
-                    <th>Apellidos</th>
-                </tr>
-            </thead>
-            <tbody>
-                { results.map( (estudiantes) => (
-                    <tr key={estudiantes.id}>
-                        <td>{estudiantes.nombres}</td>
-                        <td>{estudiantes.apellidos}</td>
-                    </tr>                    
-                ))}
-            </tbody>
-        </table>
-    </div>
-     );
+            <input
+                value={search}
+                onChange={searcher}
+                type="text"
+                placeholder="Search"
+                className="form-control"
+            />
+
+            <table className="table table-striped table-hover mt-5 shadow-lg">
+                <thead>
+                    <tr className="bg-curso text-white">
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {estudiantes.map(estudiante => (
+                        <tr key={estudiante.id}>
+                            <td>{estudiantes.nombres}</td>
+                            <td>{estudiantes.apellidos}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default BuscadorEstudiantes;
