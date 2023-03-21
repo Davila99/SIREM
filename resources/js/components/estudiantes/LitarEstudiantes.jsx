69e5a7a94039a74bcf0f0beda6713171abd12a81
Davila99 <davilaeliseo453@gmail.com>
2023-03-20 20:54:48 -0600

    Iterando datos de la vista estudiantes

--- resources/js/components/estudiantes/LitarEstudiantes.jsx
+++ resources/js/components/estudiantes/LitarEstudiantes.jsx
@@ -1,18 +1,50 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export const LitarEstudiantes = () => {
     const [estudiantes, setEstudiantes] = useState([]);
 
-    const getStudiantes = async () => {
-        console.log(" estoy llamando a los estudiantes");
-    }
+    //función para traer los datos de la API
+    const URL = "http://127.0.0.1:8000/search-estudiantes";
 
-
-    useEffect(() => {
-        getStudiantes()
-    }, [])
+    const getEstudiantes = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setEstudiantes(data.estudiantes.data);
+     
+        
+    };
+    console.log(estudiantes)
+    // const getEstudiantes = async () => {
+    //     const respuesta = await axios.get(URL);
+    //     setEstudiantes(respuesta.data);
+    //     console.log(respuesta.data)
+    // };
     
+    useEffect(() => {
+        getEstudiantes();
+        
+    }, []);
 
-    return <div>Listart estudiantes</div>;
+    return (
+        <div>
+            <table className="table table-striped table-hover mt-5 shadow-lg">
+                <thead>
+                    <tr className="bg-curso text-white">
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   
+                    {estudiantes.map((estudiante) => (
+                        <tr key={estudiante.id}>
+                        <td>{estudiante.nombres}</td>
+                        <td>{estudiante.apellidos}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
