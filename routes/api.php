2f9c620bf8ffa4e5f4a9644275c670c3efa1ba52
Davila99 <davilaeliseo453@gmail.com>
2023-03-24 20:40:19 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(BuscadorEstudiante::class)->group(function () {
-    Route::get('/estudiantes', 'index');
-    Route::post('/estudiante', 'store');
-    Route::get('/estudiante/{id}', 'show');
-    Route::post('/estudiante/{id}', 'update');
-    Route::post('/estudiante/{id}', 'destroy');
-   
-});
+
