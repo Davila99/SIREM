d8a9573d6e80d272a687b3402b9d6a4557ff20eb
Davila99 <davilaeliseo453@gmail.com>
2023-03-27 20:25:53 -0600

    Rendirisando components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,19 @@ require('./bootstrap');
 import App from './components/app'
 
 import 'sweetalert2/src/sweetalert2.scss'
+import BuscadorEmpleados from "./components/empleados/BuscadorEmpleados";
+import { CrearEstudiante } from "./components/estudiantes/CrearEstudiante";
+import BuscadorEstudiantes from "./components/estudiantes/BuscadorEstudiantes";
 //importar sweet alert dialog en las vistas eliminar/confirmar
  window.Swal = require('sweetalert2')
- const appReact = document.getElementById('reactapp');
- const root = ReactDOM.createRoot(appReact);
 
- root.render(<App />);
+ if (document.getElementById('reactapp')) {
+    const appReact = document.getElementById('reactapp');
+    const root = ReactDOM.createRoot(appReact);
+    root.render(<BuscadorEstudiantes/>);
+  }
+  if (document.getElementById('reactAppBuscadorEmpleado')) {
+    const appReact = document.getElementById('reactAppBuscadorEmpleado');
+    const root = ReactDOM.createRoot(appReact);
+    root.render(<BuscadorEmpleados/>);
+  }
