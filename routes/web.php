cefd454bfbf11224edc7279c9c535a343c634d09
Davila99 <davilaeliseo453@gmail.com>
2023-03-27 18:56:05 -0600

    integrando las rutas en el middleware,

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AsignaturaController;
 use App\Http\Controllers\AsignaturaDocenteController;
+use App\Http\Controllers\BuscadorEmpledado;
 use App\Http\Controllers\BuscadorEstudiante;
 use App\Http\Controllers\CalificacionesController;
 use App\Http\Controllers\CargoController;
@@ -36,47 +37,57 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth/login');
-}); 
+});
 
-Route::resource('cargos', CargoController::class);
-Route::resource('nivelacademic', NivelesAcademicoController::class);
-Route::resource('grados', GradoController::class);
-Route::resource('profession', ProfessionController::class);
-Route::resource('asignaturas', AsignaturaController::class);
-Route::resource('cevaluativos', CortesEvaluativoController::class);
-Route::resource('tmatricula', TipoMatriculaController::class);
-Route::resource('tutores', TutoreController::class);
-Route::resource('consanguiniedades', ConsanguiniedadeController::class);
-Route::resource('empleados', EmpleadoController::class);
-Route::resource('estudiantes', EstudianteController::class);
-Route::get('search/', [EstudianteController::class, 'busqueda']);
-Route::resource('empleados', EmpleadoController::class);
-Route::resource('tutores', TutoreController::class);
-Route::resource('consanguiniedades', ConsanguiniedadeController::class);
-Route::resource('asignaturadocente', AsignaturaDocenteController::class);
-Route::resource('matriculas', MatriculaController::class);
-Route::resource('grupos', GruposController::class);
-Route::resource('calificaciones', CalificacionesController::class);
-Route::resource('tutorestudiante', EstudiantesTutoresController::class);
-Route::resource('organizacionacademica', OrganizacionAcademicaController::class);
-Route::get('changeStatus/', [OrganizacionAcademicaController::class, 'changeStatus']);
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-Route::get('buscar-estudiantes', [EstudianteController::class, 'search']);
-Route::get('buscar-grupos', [GruposController::class, 'search']);
-Route::get('buscador-tipo-matriculas', [TipoMatriculaController::class, 'search']);
-
+Route::get('/home', [
+    App\Http\Controllers\HomeController::class,
+    'index',
+])->name('home');
 
 Route::middleware(['auth'])->group(function () {
+    Route::resource('cargos', CargoController::class);
+    Route::resource('nivelacademic', NivelesAcademicoController::class);
+    Route::resource('grados', GradoController::class);
+    Route::resource('profession', ProfessionController::class);
+    Route::resource('asignaturas', AsignaturaController::class);
+    Route::resource('cevaluativos', CortesEvaluativoController::class);
+    Route::resource('tmatricula', TipoMatriculaController::class);
+    Route::resource('tutores', TutoreController::class);
+    Route::resource('consanguiniedades', ConsanguiniedadeController::class);
+    Route::resource('empleados', EmpleadoController::class);
+    Route::resource('estudiantes', EstudianteController::class);
+    Route::resource('empleados', EmpleadoController::class);
+    Route::resource('tutores', TutoreController::class);
+    Route::resource('consanguiniedades', ConsanguiniedadeController::class);
+    Route::resource('asignaturadocente', AsignaturaDocenteController::class);
+    Route::resource('matriculas', MatriculaController::class);
+    Route::resource('grupos', GruposController::class);
+    Route::resource('calificaciones', CalificacionesController::class);
+    Route::resource('tutorestudiante', EstudiantesTutoresController::class);
+    Route::resource(
+        'organizacionacademica',
+        OrganizacionAcademicaController::class
+    );
+    Route::get('changeStatus/', [
+        OrganizacionAcademicaController::class,
+        'changeStatus',
+    ]);
+    Route::get('buscar-estudiantes', [EstudianteController::class, 'search']);
+    Route::get('buscar-grupos', [GruposController::class, 'search']);
+    Route::get('buscador-tipo-matriculas', [
+        TipoMatriculaController::class,
+        'search',
+    ]);
+
+    Route::get('search/', [EstudianteController::class, 'busqueda']);
     Route::controller(BuscadorEstudiante::class)->group(function () {
-        Route::get('/search-estudiantes', 'index' );
+        Route::get('/search-estudiantes', 'index');
         Route::post('/search-estudiantes', 'store');
         Route::get('/search-estudiantes/{id}', 'show');
         Route::post('/search-estudiantes/{id}', 'update');
         Route::post('/search-estudiantes/{id}', 'destroy');
-       
     });
+    Route::get('/search-emplados', [BuscadorEmpledado::class, 'index']);
 });
