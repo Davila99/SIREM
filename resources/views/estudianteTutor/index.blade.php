446f9724a14cb82e6b4b290486ccbd3e54ad26a7
Davila99 <davilaeliseo453@gmail.com>
2023-04-03 21:15:36 -0600

    ingrando alertsweet en la vista de estudinates tutores

--- resources/views/estudianteTutor/index.blade.php
+++ resources/views/estudianteTutor/index.blade.php
@@ -47,3 +47,60 @@
     </div>
 
 @endsection
+@section('js')
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @if (Session::has('mensaje'))
+        <script>
+            Swal.fire({
+                position: 'top-center',
+                icon: 'success',
+                title: 'Estudiante registrado!',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        </script>
+    @endif
+    @if (Session::has('mensaje-editar'))
+        <script>
+            Swal.fire({
+                position: 'top-center',
+                icon: 'success',
+                title: 'Estudiante editado exitosamente!',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        </script>
+    @endif
+    @if (Session::has('mensaje-eliminar'))
+        <script>
+            Swal.fire(
+                'Eliminado!',
+                'Su archivo ha sido eliminado.',
+                'success'
+            )
+        </script>
+    @endif
+    <script>
+        $('.form-eliminar').submit(function(e) {
+            e.preventDefault();
+            Swal.fire({
+                title: '¿Estas seguro?',
+                text: "No podrás revertir esto.!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si eliminar',
+                cancelButtonText: 'Calcelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.submit();
+                }
+            })
+        });
+    </script>
+@endsection
