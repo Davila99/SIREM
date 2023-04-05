a525ee94537045e6970a46c04fd59b42dba208de
Davila99 <davilaeliseo453@gmail.com>
2023-04-05 09:21:38 -0600

    Update Tutore.php

--- app/Models/Tutore.php
+++ app/Models/Tutore.php
@@ -13,8 +13,4 @@ class Tutore extends Model
     {
         return $this->belongsTo(Profession::class,'professions_id','id');
     }
-    public function estudiante()
-    {
-        return $this->hasMany(Estudiante::class,'id');
-    }
 }
