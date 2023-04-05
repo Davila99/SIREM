5c9242f7f0d23fb0936ed12b04be8c1d682fb1fd
Davila99 <davilaeliseo453@gmail.com>
2023-04-05 07:33:21 -0600

    mejorando experiencia de UI en las vista Form de Estudiantes Tutores

--- resources/views/estudianteTutor/form.blade.php
+++ resources/views/estudianteTutor/form.blade.php
@@ -12,36 +12,46 @@
             <th>
                 <div class="form-group">
                     <select class="form-control @error('estudiante_id') is-invalid @enderror" name="estudiante_id"
-                        id="estudiante">
-                        <option value="" selected disabled>--Seleccione--</option>
+                    id="estudiante">
+                    <option value="" selected disabled>--Seleccione--</option>
 
-                        @isset($estudiantes)
-                            @foreach ($estudiantes as $estudiante)
-                                <option value="{{ $estudiante->id }}"
-                                    {{ old('estudiante_id') == $estudiante->id ? 'selected' : '' }}>
-                                    {{ $estudiante->nombres }}</option>
-                            @endforeach
-                        @endisset
+                    @isset($estudiantes)
+                        @foreach ($estudiantes as $estudiante)
+                            <option value="{{ $estudiante->id }}"
+                                @if (!empty($datos->estudiante_id)) {{ $datos->estudiante_id == $estudiante->id ? 'selected' : '' }} @else {{ old('estudiante_id') == $estudiante->id ? 'selected' : '' }} @endif>
+                                {{ $estudiante->nombres }} </option>
+                        @endforeach
+                    @endisset
 
+                </select>
+                @error('estudiante_id')
+                    <div class="invalid-feedback">
+                        <h5> {{ $message }}</h5>
+                    </div>
+                @enderror
                 </div>
             </th>
             <th>
-
                 <div class="form-group">
                     <select class="form-control @error('tutores_id') is-invalid @enderror" name="tutores_id"
-                        id="tutor">
+                    id="tutores">
+                    <option value="" selected disabled>--Seleccione--</option>
 
-                        <option value="" selected disabled>--Seleccione--</option>
+                    @isset($tutores)
+                        @foreach ($tutores as $tutor)
+                            <option value="{{ $tutor->id }}"
+                                @if (!empty($datos->tutores_id)) {{ $datos->tutores_id == $tutor->id ? 'selected' : '' }} @else {{ old('tutores_id') == $tutor->id ? 'selected' : '' }} @endif>
+                                {{ $tutor->nombre }} </option>
+                        @endforeach
+                    @endisset
 
-                        @isset($tutores)
-                            @foreach ($tutores as $tutore)
-                                <option value="{{ $tutore->id }}"
-                                    {{ old('tutores_id') == $tutore->id ? 'selected' : '' }}>{{ $tutore->nombre }}</option>
-                            @endforeach
-                        @endisset
-
+                </select>
+                @error('tutores_id')
+                    <div class="invalid-feedback">
+                        <h5> {{ $message }}</h5>
+                    </div>
+                @enderror
                 </div>
-
             </th>
         </tr>
     </tbody>
