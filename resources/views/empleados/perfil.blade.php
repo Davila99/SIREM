a33956514dd71d53b474d7106a9a064dff8dde33
Davila99 <davilaeliseo453@gmail.com>
2023-04-08 22:29:01 -0600

    Update perfil.blade.php

--- resources/views/empleados/perfil.blade.php
+++ resources/views/empleados/perfil.blade.php
@@ -14,11 +14,6 @@
                             <div class="col-sm-4 col-md-3">
                                 <div class="card bg-light">
                                     <div class="px-5 pt-2">
-                                        {{-- @if (isset($teacher->photo))
-                                            <img src="{{asset('/storage'.$tutor->photo)}}" class="rounded-3 card-img-top" alt="Profile photo">
-                                        @else
-                                            <img src="{{asset('imgs/profile.png')}}" class="rounded-3 card-img-top" alt="Profile photo">
-                                        @endif --}}
                                     </div>
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $empleado->nombres}} {{$empleado->apellidos}}</h5>
