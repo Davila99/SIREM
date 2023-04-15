b02396f3cf12031c7a586e188253b7bb0aad10e7
Davila99 <davilaeliseo453@gmail.com>
2023-04-15 09:13:50 -0600

    Update perfil.blade.php

--- resources/views/tutores/perfil.blade.php
+++ resources/views/tutores/perfil.blade.php
@@ -6,25 +6,11 @@
         <div class="col-xs-11 col-sm-11 col-md-11 col-lg-10 col-xl-10 col-xxl-10">
             <div class="row pt-2">
                 <div class="col ps-4">
-                    <h1 class="display-6 mb-3">
-                        <i class="bi bi-person-lines-fill"></i> Información de  Tutor
-                    </h1>
                     <div class="mb-4">
                         <div class="row">
-                            <div class="col-sm-4 col-md-3">
-                                <div class="card bg-light">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $tutores->nombre}} {{$tutores->apellido}}</h5>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Cedula: {{$tutores->cedula }}</li>
-                                        <li class="list-group-item">Telefono: {{$tutores->telefono}}</li>
-                                    </ul>
-                                </div>
-                            </div>
                             <div class="col-sm-8 col-md-9">
                                 <div class="p-3 mb-3 border rounded bg-white">
-                                    <h6>Tutor </h6>
+                                    <h4>Informacion del tutor </h4>
                                     <table class="table table-responsive mt-3">
                                         <tbody>
                                             <tr>
