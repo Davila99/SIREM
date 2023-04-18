ad67a300181c6d6370f8437c2d0bb6f4a331052c
Davila99 <davilaeliseo453@gmail.com>
2023-04-18 14:24:35 -0600

    Revert "elimiando dato non requeridos"
    This reverts commit eef3476e504029a9985efd79fae274cf412b50fe.

---
+++ resources/views/cargo/form.blade.php
@@ -0,0 +1,24 @@
+
+    <fieldset class="border p-4">
+        <div class="form-group">
+            <label for="descripcion"><h5>Nombre del Cargo:</h5></label>
+            <input id="descripcion" name="descripcion" type="text"
+                class="form-control @error('descripcion') is-invalid @enderror" placeholder="Cargo" 
+                value="{{ isset($datos->descripcion) ? $datos->descripcion : old('descripcion') }}">
+            @error('descripcion')
+                <div class="invalid-feedback">
+                    <h5> {{ $message }}</h5>
+                </div>
+            @enderror
+        </div>
+    </fieldset>
+
+        
+    <div class="d-grid mt-1 col-sm-1">
+        <input type="submit" value="Guardar" class="btn btn-success">
+    </div>
+
+    <div class="d-grid mt-1 col-sm-10">
+        <a type="button" class="btn btn-primary" href="{{ url('cargos/') }}"> Regresar </a>
+    </div>
+    </div>
---
+++ resources/views/cargo/index.blade.php
@@ -0,0 +1,111 @@
+ @extends('adminlte::page')
+
+ @section('content')
+     <div class="container">
+         <br>
+         <a href="{{ url('cargos/create') }}" class="btn btn-success"> Nuevo Cargo </a>
+         <br>
+         <br>
+         <div class="table-responsive">
+             <table class="table table-dark">
+                 <thead class="thead-light">
+                     <tr>
+                         <th>Cargos</th>
+                         <th>Acciones</th>
+                     </tr>
+                 </thead>
+
+                 <tbody>
+
+                     @foreach ($cargos as $cargo)
+                         <tr>
+                             <td>{{ $cargo->descripcion }}</td>
+
+                             <td>
+                                 <div class="d-flex flex-row bd-highlight mb-6">
+
+                                     <a href="{{ url('/cargos/' . $cargo->id . '/edit') }}" class="btn btn-info">
+                                         Editar </a>|
+                                     <form class="form-eliminar" action="{{ url('/cargos/' . $cargo->id) }}" method="post"
+                                         class="d-inline">
+                                         @csrf
+                                         {{ method_field('DELETE') }}
+                                         <button type="submit" class="btn btn-danger">Eliminar</button>
+                                     </form>
+                                 </div>
+
+                             </td>
+
+                         </tr>
+                     @endforeach
+                 </tbody>
+             </table>
+         </div>
+
+     </div>
+ @stop
+ @section('js')
+     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+     @if (Session::has('mensaje'))
+         <script>
+             Swal.fire({
+                 position: 'top-center',
+                 icon: 'success',
+                 title: 'Cargo registrado!',
+                 showConfirmButton: false,
+                 timer: 2000
+             })
+         </script>
+          @endif
+
+          @if (Session::has('mensaje-error-eliminar'))
+              <script>
+                  Swal.fire({
+                      icon: 'error',
+                      title: 'Lo sentimos',
+                      text: 'Este dato esta siendo utilizado',
+                  })
+              </script>
+     @endif
+     @if (Session::has('mensaje-editar'))
+         <script>
+             Swal.fire({
+                 position: 'top-center',
+                 icon: 'success',
+                 title: 'Cargo editado exitosamente!',
+                 showConfirmButton: false,
+                 timer: 2000
+             })
+         </script>
+     @endif
+     @if (Session::has('mensaje-eliminar'))
+         <script>
+             Swal.fire(
+                 'Eliminado!',
+                 'Su archivo ha sido eliminado.',
+                 'success'
+             )
+         </script>
+     @endif
+     <script>
+         $('.form-eliminar').submit(function(e) {
+             e.preventDefault();
+             Swal.fire({
+                 title: '¿Estas seguro?',
+                 text: "No podrás revertir esto.!",
+                 icon: 'warning',
+                 showCancelButton: true,
+                 confirmButtonColor: '#3085d6',
+                 cancelButtonColor: '#d33',
+                 confirmButtonText: 'Si eliminar',
+                 cancelButtonText: 'Calcelar'
+             }).then((result) => {
+                 if (result.isConfirmed) {
+
+                     this.submit();
+                 }
+             })
+
+         });
+     </script>
+ @endsection
