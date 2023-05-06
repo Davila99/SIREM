e0541bd5275eedf77b0e6668593fe4a320220940
Davila99 <davilaeliseo453@gmail.com>
2023-05-05 21:04:39 -0600

    creando componente con livewire

---
+++ app/Http/Livewire/Admin/UsersIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class UsersIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.users-index');
+    }
+}
