560adc39d81582621c7ab46f5c9704fc8955b287
Davila99 <davilaeliseo453@gmail.com>
2023-05-15 20:05:37 -0600

    Creando el modelo de Seccion

---
+++ app/Http/Controllers/SeccionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seccion;
+use App\Http\Requests\StoreSeccionRequest;
+use App\Http\Requests\UpdateSeccionRequest;
+
+class SeccionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSeccionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSeccionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seccion $seccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Seccion $seccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSeccionRequest  $request
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSeccionRequest $request, Seccion $seccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seccion $seccion)
+    {
+        //
+    }
+}
