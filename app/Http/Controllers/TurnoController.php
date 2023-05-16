79f780c4fde2c7632b09bd73d64da36c827153b6
Davila99 <davilaeliseo453@gmail.com>
2023-05-15 21:06:42 -0600

    creando elementos necesarios para Turnoi

---
+++ app/Http/Controllers/TurnoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Turno;
+use App\Http\Requests\StoreTurnoRequest;
+use App\Http\Requests\UpdateTurnoRequest;
+
+class TurnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTurnoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTurnoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Turno  $turno
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Turno $turno)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Turno  $turno
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Turno $turno)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTurnoRequest  $request
+     * @param  \App\Models\Turno  $turno
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTurnoRequest $request, Turno $turno)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Turno  $turno
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Turno $turno)
+    {
+        //
+    }
+}
