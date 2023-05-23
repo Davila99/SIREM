60be62690c77b60d4c01cf16a88b9d9058442fc6
Davila99 <davilaeliseo453@gmail.com>
2023-05-22 19:38:54 -0600

    Update detalles.blade.php

--- resources/views/asignaturadocente/detalles.blade.php
+++ resources/views/asignaturadocente/detalles.blade.php
@@ -6,71 +6,69 @@
                 <div class="row pt-2">
                     <div class="col ps-4">
                         <h1 class="display-6 mb-3">
-                            <i class="bi bi-person-lines-fill"></i> Informacion de Matricula
+                            <i class="bi bi-person-lines-fill"></i> Informacion de asignacion docente 
                         </h1>
                         <div class="mb-4">
                             <div class="col-sm-8 col-md-9">
                                 <div class="p-3 mb-3 border rounded bg-white">
-                                    <h6>Informacion de Estudiante  </h6>
+                                    <h6>Informacion de empleado </h6>
                                     <table class="table table-responsive mt-3">
                                         <tbody>
                                             <tr>
                                                 <th scope="row">Nombres:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->nombres }}</td>
+                                                <td>{{$asignaturaDocente->empleado->nombres}}</td>
                                                 <th>Apellidos:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->apellidos }}</td>
+                                                <td>{{$asignaturaDocente->empleado->apellidos}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Telefono:</th>
+                                                <td>{{$asignaturaDocente->empleado->telefono}}</td>
+                                                <th scope="row">Cedula:</th>
+                                                <td>{{$asignaturaDocente->empleado->cedula}}</td>
+                                            </tr>
+                                            <tr>
                                             </tr>
                                             <tr>
                                                 <th scope="row">Fecha de Nacimiento:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->fecha_nacimiento }}</td>
-                                                <th scope="row">Edad:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->edad }}</td>
+                                                <td>{{$asignaturaDocente->empleado->fecha_nacimiento}}</td>
+                                                <th>Nivel Academico:</th>
+                                                <td>{{$asignaturaDocente->empleado->nivel_academico->descripcion}}</td>
                                             </tr>
                                             <tr>
+                                                <th scope="row">Direccion:</th>
+                                                <td>{{$asignaturaDocente->empleado->direccion}}</td>
+                                                <th>Email:</th>
+                                                <td>{{$asignaturaDocente->empleado->email}}</td>
                                             </tr>
                                             <tr>
-                                                <th scope="row">Direcion:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->direccion }}</td>
-                                                <th>Tutor:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->tutor->nombre}}</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Sexo:</th>
-                                                <td>{{ $asignaturaDocente->estudiante->sexo->descripcion }}</td>
-                                                <th>Tipo de Matricula:</th>
-                                                <td>{{ $asignaturaDocente->tipo_matricula->descripcion}}</td>
+                                                <th scope="row">Fecha de ingreso</th>
+                                                <td>{{$asignaturaDocente->empleado->fecha_ingreso}}</td>
+                                                <th>Cargo:</th>
+                                                <td>{{$asignaturaDocente->empleado->cargos->descripcion}}</td>
                                             </tr>
                                             <tr>
                                             </tr>
                                         </tbody>
                                     </table>
-                                </div>
+                                </div> 
                             </div>
 
                             <div class="col-sm-8 col-md-9">
                                 <div class="p-3 mb-3 border rounded bg-white">
-                                    <h6>Informacion del grupo </h6>
+                                    <h6>Informacion  </h6>
                                     <table class="table table-responsive mt-3">
                                         <tbody>
                                             <tr>
-                                                <th scope="row">Nombres:</th>
-                                                <td>{{ $asignaturaDocente->tipo_matricula }}</td>
-                                                <th>Apellidos:</th>
-                                                <td>{{ $asignaturaDocente->tipo_matricula }}</td>
+                                                <th scope="row">Seccion:</th>
+                                                <td>{{ $asignaturaDocente->seccion->descripcion }}</td>
+                                                <th>Turno:</th>
+                                                <td>{{ $asignaturaDocente->turno->descripcion }}</td>
                                             </tr>
                                             <tr>
-                                                <th scope="row">Cedula:</th>
-                                                <td>{{ $asignaturaDocente->tipo_matricula }}</td>
-                                                <th scope="row">Telefono:</th>
-                                                <td>{{ $asignaturaDocente->tipo_matricula }}</td>
-                                            </tr>
-                                            <tr>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Profesion:</th>
-                                                <td>{{ $asignaturaDocente->tipo_matricula }}</td>
-                                            </tr>
-                                            <tr>
+                                                <th scope="row">Asignatura:</th>
+                                                <td>{{ $asignaturaDocente->asignatura->descripcion }}</td>
+                                                <th scope="row">Grado:</th>
+                                                <td>{{ $asignaturaDocente->grado->descripcion }}</td>
                                             </tr>
                                         </tbody>
                                     </table>
@@ -78,29 +76,30 @@
                             </div>
                             <div class="col-sm-8 col-md-9">
                                 <div class="p-3 mb-3 border rounded bg-white">
-                                    <h6>Informacion de Tutor </h6>
+                                    <h6>Informacion de organizacion </h6>
                                     <table class="table table-responsive mt-3">
                                         <tbody>
                                             <tr>
-                                                <th scope="row">Nombres:</th>
-                                                <td>{{ $asignaturaDocente->tutores }}</td>
-                                                <th>Apellidos:</th>
-                                                <td>{{ $asignaturaDocente->tutores }}</td>
+                                                <th scope="row">Organizacion:</th>
+                                                <td>{{ $asignaturaDocente->organizacionAcademica->descripcion }}</td>
+                                                <th>Fecha:</th>
+                                                <td>{{ $asignaturaDocente->organizacionAcademica->fecha }}</td>
                                             </tr>
                                             <tr>
-                                                <th scope="row">Cedula:</th>
-                                                <td>{{ $asignaturaDocente->tutores }}</td>
-                                                <th scope="row">Telefono:</th>
-                                                <td>{{ $asignaturaDocente->tutores }}</td>
-                                            </tr>
-                                            <tr>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Profesion:</th>
-                                                <td>{{ $asignaturaDocente->tutores }}</td>
-                                            </tr>
-                                            <tr>
-                                            </tr>
+                                                <th scope="row">Estado:</th>
+                                                <td>
+                                                    <div class="status-bar">
+                                                        <div class="status-bar">
+                                                            @if($asignaturaDocente->organizacionAcademica->confirmed==true)
+                                                                    <i class="far fa-thumbs-up"></i>          
+                                                            @else
+                                                                <i class="far fa-thumbs-down"></i>
+                                                            @endif
+                                                        </div>
+                                                        
+                                                    </div>
+                                                    
+                                                </td>
                                         </tbody>
                                     </table>
                                 </div>
