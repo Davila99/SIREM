d9828edeacbe754bab28dc42e803864ba70eace2
Davila99 <davilaeliseo453@gmail.com>
2023-06-06 17:20:11 -0600

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,7 +27,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Contraseña" placeholder="Nombre Completo">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nombre Completo">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
