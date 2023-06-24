5ac3774ac5ce1700a7d9be4c9744bba9934ecf38
Davila99 <davilaeliseo453@gmail.com>
2023-06-24 08:40:13 -0600

    cambios

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -1,60 +1,61 @@
 <div>
+    <br>
+    <a href="{{ url('register') }}" class="btn btn-success"> Nuevo Usuario </a>
+    <br>
     <div class="card">
         <div class="card-header">
-            <input wire:model='search'  class="form-control" placeholder="Ingrese su nombre o su correo electronico">
+            <input wire:model='search' class="form-control" placeholder="Ingrese su nombre o su correo electronico">
         </div>
         @if ($users->count())
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead class="thead-light">
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Email</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-        
-                        @foreach ($users as $user)
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead class="thead-light">
                             <tr>
-                                <td>{{ $user->id }}</td>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>
-                                    <div class="d-flex flex-row bd-highlight mb-6">
-                                        <a href="{{ url('/users/' . $user->id . '/edit') }}"
-                                            class="btn btn-info">
-                                            Editar </a>|
-                                        <form class="form-eliminar"
-                                            action="{{ url('/users/' . $user->id) }}" method="post"
-                                            class="d-inline">
-                                            @csrf
-                                            {{ method_field('DELETE') }}
-                                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                                        </form>
-                                    </div>
-                                </td>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Acciones</th>
                             </tr>
-                        @endforeach
-                        @livewireScripts
-                    </tbody>
-                </table>
-            </div>       
+                        </thead>
+                        <tbody>
 
-      </div>
-      
-      <div class="card-footer">
-        {{ $users->links() }}
-      </div>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>
+                                        <div class="d-flex flex-row bd-highlight mb-6">
+                                            <a href="{{ url('/users/' . $user->id . '/edit') }}" class="btn btn-info">
+                                                Editar </a>|
+                                            <form class="form-eliminar" action="{{ url('/users/' . $user->id) }}"
+                                                method="post" class="d-inline">
+                                                @csrf
+                                                {{ method_field('DELETE') }}
+                                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            @livewireScripts
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+            <div class="card-footer">
+                {{ $users->links() }}
+            </div>
         @else
-        <div class="card-body">
-            <strong>No hay registro</strong>
-        </div>
+            <div class="card-body">
+                <strong>No hay registro</strong>
+            </div>
 
         @endif
-       
-    
-       
-</div>
+
+
+
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth/login');
 });
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
-Auth::routes();
+
+
 
 Route::get('/home', [
     App\Http\Controllers\HomeController::class,
@@ -50,7 +54,16 @@ Route::get('/home', [
 ])->name('home');
 
 Route::middleware(['auth'])->group(function () {
+    // Rutas de registro
+    Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
 
+    // Rutas de restablecimiento de contraseña
+    Route::get('/password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('/password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('/password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+    
     Route::resource('users', UserController::class);
 
     Route::get('mi-docencia', [MiDocenciaController::class, 'index']);
