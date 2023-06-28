39bda1dc44c8f8ea9e58149798256f510df114a5
Davila99 <davilaeliseo453@gmail.com>
2023-06-28 17:54:30 -0600

    Update Grado.php

--- app/Models/Grado.php
+++ app/Models/Grado.php
@@ -9,16 +9,6 @@ class Grado extends Model
 {
     use HasFactory;
 
-    // public function gradoGrupo()
-    // {
-    //     return $this->hasOneThrough(
-    //         AsignaturaDocente::class,
-    //         Grupos::class,
-    //         'grado_id',
-    //         'grupo_id',
-    //         'id',
-    //         'id');
-    // }
     public function grupo()
     {
         return $this->belongsTo(Grupos::class);
