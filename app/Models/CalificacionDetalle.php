dd3d487fe5cc7abf140a9206010174fa36bafdb7
Darwin Salinas <salinash2000@gmail.com>
2023-07-11 17:51:59 -0600

    fix(CalificacionDetalle): Add relation to estudiante model

--- app/Models/CalificacionDetalle.php
+++ app/Models/CalificacionDetalle.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CalificacionDetalle extends Model
 {
     use HasFactory;
+
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiante::class);
+    }
 }
