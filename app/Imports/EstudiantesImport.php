91446654855270915a36b5bb18226ab1a5a26c75
Davila99 <davilaeliseo453@gmail.com>
2023-10-11 20:21:19 -0600

    omitiendo las cabeceras de los excel

--- app/Imports/EstudiantesImport.php
+++ app/Imports/EstudiantesImport.php
@@ -4,8 +4,9 @@ namespace App\Imports;
 
 use App\Models\Estudiante;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class EstudiantesImport implements ToModel
+class EstudiantesImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -15,10 +16,10 @@ class EstudiantesImport implements ToModel
     public function model(array $row)
     {
         return new Estudiante([
-            'id' => $row[0],
-            'nombres' => $row[1],
-            'apellidos' => $row[2],
-            'codigo_estudiante' => $row[3],
+            'id' => $row['id'],
+            'nombres' => $row['nombres'],
+            'apellidos' => $row['apellidos'],
+            'codigo_estudiante' => $row['codigo_estudiante'],
         ]);
     }
 }
