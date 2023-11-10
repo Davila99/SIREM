80ad98428926a6c76bad2e9fd90d3ef0286ff2f4
Davila99 <davilaeliseo453@gmail.com>
2023-11-09 20:04:41 -0600

    Create reporte.blade.php

---
+++ resources/views/calificaciones/reporte.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Acta Académica</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+            border: 1px solid #000;
+        }
+        .table th, .table td {
+            border: 1px solid #000;
+            padding: 8px;
+        }
+        .text-center {
+            text-align: center;
+        }
+        .font-weight-bold {
+            font-weight: bold;
+        }
+        .font-italic {
+            font-style: italic;
+        }
+    </style>
+</head>
+<body>
+    <div class="table-responsive">
+        <table class="table">
+            <thead> 
+                <tr>
+                    <th scope="col" colspan="4" class="text-center">
+                        <h1 class="font-weight-bold font-italic">Colegio Cristiano Manto de Gracia</h1>
+                    </th>
+                </tr>
+            </thead>
+            <thead>
+                <tr>
+                    <th scope="col" colspan="4" class="text-center">
+                        <h1 class="font-weight-bold font-italic">Acta Académica</h1>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Fecha de generación</th>
+                    <td>{{ $acta->fecha }}</td>
+                    <th scope="row">Empleado</th>
+                    <td>{{ $acta->empleado->nombres }} {{ $acta->empleado->apellidos }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Asignatura</th>
+                    <td>{{ $acta->asignatura->descripcion }}</td>
+                    <th scope="row">Observaciones</th>
+                    <td>{{ $acta->observaciones }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Estudiante</th>
+                    <th>Calificacion</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($filas as $fila)
+                    <tr>
+                        <td>{{ $fila->estudiante->nombres }} {{ $fila->estudiante->apellidos }}</td>
+                        <td>{{ $fila->calificacion }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
