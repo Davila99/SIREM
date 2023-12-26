5160d43b4d2d8a8bd50ea59a99201f2b414dea9d
Davila99 <davilaeliseo453@gmail.com>
2023-12-25 21:28:56 -0600

    Update edit.blade.php

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <legend class="text">Editar Usuario</legend>
-        <form action="{{ url('user/' . $datos->id) }}" method="post" enctype="multipart/form-data">
+        <form action="{{ url('users/' . $datos->id) }}" method="post" enctype="multipart/form-data">
             @csrf
             {{ method_field('PATCH') }}
             @include('user.form')
