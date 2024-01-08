aca9b6d89440d583f1d8e87682188611e6b851d8
Davila99 <davilaeliseo453@gmail.com>
2024-01-07 20:20:39 -0600

    Update ReporteCalificacionesController.php

--- app/Http/Controllers/ReporteCalificacionesController.php
+++ app/Http/Controllers/ReporteCalificacionesController.php
@@ -10,7 +10,82 @@ class ReporteCalificacionesController extends Controller
     public function index()
     {
         $datos['calificaciones'] = Calificaciones::query()
-            ->select('calificaciones.*')
+            ->select('calificaciones.fecha as FechaRegistroCalificacion')
+            ->leftJoin('grupos', 'calificaciones.grupo_id', '=', 'grupos.id')
+            ->leftJoin('grados', 'grupos.grado_id', '=', 'grados.id')
+            ->selectRaw(
+                '
+         CASE
+             WHEN grupos.grado_id IS NULL THEN "No definido"
+         ELSE grados.descripcion
+         END as Grado'
+            )
+            ->leftJoin('seccions', 'grupos.seccion_id', '=', 'seccions.id')
+            ->selectRaw(
+                '
+          CASE
+             WHEN grupos.seccion_id IS NULL THEN "No definido"
+             ELSE seccions.descripcion
+         END as Seccion'
+            )
+            ->leftJoin('turnos', 'grupos.turno_id', '=', 'turnos.id')
+            ->selectRaw(
+                '
+          CASE
+          WHEN grupos.turno_id IS NULL THEN "No definido"
+          ELSE turnos.descripcion
+          END as Turno'
+            )
+            ->selectRaw(
+                '
+            CASE
+            WHEN grupos.anio_lectivo IS NULL THEN "No definidio"
+            ELSE grupos.anio_lectivo
+            END as AnioLectivo
+    '
+            )
+            ->leftJoin(
+                'asignaturas',
+                'calificaciones.asignatura_id',
+                '=',
+                'asignaturas.id'
+            )
+            ->selectRaw(
+                '
+        CASE
+            WHEN asignaturas.descripcion IS NULL THEN "No definidio"
+            ELSE asignaturas.descripcion
+        END as Asignatura
+    '
+            )
+            ->leftJoin(
+                'cortes_evaluativos',
+                'calificaciones.corte_evaluativo_id',
+                '=',
+                'cortes_evaluativos.id'
+            )
+            ->selectRaw(
+                '
+        CASE
+            WHEN cortes_evaluativos.descripcion IS NULL THEN "No definidio"
+            ELSE cortes_evaluativos.descripcion
+        END as Corte
+    '
+            )
+            ->leftJoin(
+                'calificacion_detalles',
+                'calificacion_detalles.calificacion_id',
+                '=',
+                'calificacion_detalles.id'
+            )
+            ->selectRaw(
+                '
+        CASE
+            WHEN calificacion_detalles.calificacion IS NULL THEN "No definidio"
+            ELSE calificacion_detalles.calificacion
+        END as Calificacion
+    '
+            )
             ->get();
         return response()->json($datos);
     }
