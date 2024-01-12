0a84263bb80ae6dbcaf28aea8391028db2965c0f
Ana-canales <anarojascanales2730@gmail.com>
2024-01-11 18:29:28 -0600

    mejorando el panel dashboar de matriculas

--- resources/views/matriculas/create.blade.php
+++ resources/views/matriculas/create.blade.php
@@ -1,14 +1,13 @@
 @extends('adminlte::page')
+
 @section('content')
+    @include('layouts.app') {{-- Incluir el contenido del segundo layout --}}
+
     <div class="container">
         <legend class="text">Registrar Matricula</legend>
         <form action="{{ url('/matriculas') }}" method="post" enctype="multipart/form-data">
             @csrf
             @include('matriculas.form')
-
         </form>
-       
     </div>
 @endsection
-
-
